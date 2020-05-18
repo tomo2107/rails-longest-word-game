f8d82284f3e43e816ad2053b3035f02da66ad1a4
Torsten Motzkus <tmotzkus@web.de>
2020-05-18 22:33:05 +0200

    Solution challenge 4

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,24 @@
+class GamesController < ApplicationController
+  def new
+    @letters = (0..10).map { (65 + rand(26)).chr }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    words.chars.all? {|letter| word.counter(letter) <= letters.count(letter)}
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
